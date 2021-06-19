c8e267465f062a22641d4635c7abcb2b3ced3939
YanaEgorova <yanaegorova69.3@gmail.com>
2021-06-20 00:54:30 +0300

    +

--- js/sliders.js
+++ js/sliders.js
@@ -1,5 +1,9 @@
 var swiper = new Swiper('.swiper-container-www.swiper-container', {
   slidesPerView: 'auto',
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   1280: {
     noSwiping: true,
     noSwipingClass: 'swiper-container',
